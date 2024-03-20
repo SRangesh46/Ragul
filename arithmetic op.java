171af9173d2e9f905a368ad41c64036e510c3836
Rakesh-43 <159746473+Rakesh-43@users.noreply.github.com>
2024-03-20 11:25:01 +0530

    Update arithmetic op.java

--- arithmetic op.java
+++ arithmetic op.java
@@ -1,9 +1,9 @@
 
-public class SumOfNumbers1.
+public class subtractOfNumbers1.
 {
 public static void main(String args[])
 {
-int n1 = 225, n2 = 115, sum;
-sum = n1 + n2;
-System.out.println("The sum of numbers is: "+sum);
+int n1 = 225, n2 = 115, sub;
+sub = n1 - n2;
+System.out.println("The subtract of numbers is: "+sub);
 }
