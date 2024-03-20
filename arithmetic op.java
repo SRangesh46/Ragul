18d42011255bfbf8997cca711f930ae2fc2d8242
Ragulramachandiran <164068809+Ragulramachandiran@users.noreply.github.com>
2024-03-20 09:56:03 +0530

    Update arithmetic op.java

--- arithmetic op.java
+++ arithmetic op.java
@@ -1,1 +1,21 @@
+// Java code to illustrate "=" operator
+
+import java.io.*;
+
+class Assignment {
+	public static void main(String[] args)
+	{
+		// Declaring variables
+		int num;
+		String name;
+
+		// Assigning values
+		num = 10;
+		name = "GeeksforGeeks";
+
+		// Displaying the assigned values
+		System.out.println("num is assigned: " + num);
+		System.out.println("name is assigned: " + name);
+	}
+}
 
