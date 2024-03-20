792c6dd90f62122aef87b325a4f3a3a321862674
Ragulramachandiran <164068809+Ragulramachandiran@users.noreply.github.com>
2024-03-20 11:31:18 +0530

    Update arithmetic op.java

--- arithmetic op.java
+++ arithmetic op.java
@@ -1,9 +1,9 @@
 
-public class multiplyOfNumbers1.
+public class divideOfNumbers1.
 {
 public static void main(String args[])
 {
-int n1 = 225, n2 = 115, mul;
-mul = n1 × n2;
-System.out.println("The multiply of numbers is: "+sub);
+int n1 = 225, n2 = 115, div;
+div = n1 × n2;
+System.out.println("The divide of numbers is: "+sub);
 }
