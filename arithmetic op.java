258a64068f9f9a96835906c6baa171461e39377d
bharathielumalai5335 <159746627+bharathielumalai5335@users.noreply.github.com>
2024-03-20 11:30:14 +0530

    Update arithmetic op.java

--- arithmetic op.java
+++ arithmetic op.java
@@ -1,9 +1,9 @@
 
-public class subtractOfNumbers1.
+public class multiplyOfNumbers1.
 {
 public static void main(String args[])
 {
-int n1 = 225, n2 = 115, sub;
-sub = n1 - n2;
-System.out.println("The subtract of numbers is: "+sub);
+int n1 = 225, n2 = 115, mul;
+mul = n1 × n2;
+System.out.println("The multiply of numbers is: "+sub);
 }
